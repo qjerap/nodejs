7d45adb36bd637ba29fbcfc838598532af93f5b4
RP <romainpareja@gmail.com>
2019-02-03 10:59:12 -0500

    completed routes

--- routes/admin.js
+++ routes/admin.js
@@ -7,8 +7,11 @@ const adminController = require('../controllers/admin');
 
 router.get('/add-product', adminController.getAddProduct);
 router.get('/products', adminController.getProducts);
+router.get('/edit-product/:productId', adminController.getEditProduct)
 
 router.post('/add-product', adminController.postAddProduct);
+router.post('/edit-product', adminController.postEditProduct);
+router.post('/delete-product/:productId', adminController.postDeleteProduct);
 
 
 module.exports = router;
